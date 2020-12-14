71fa1054fdcb368f091264ede4150f19617d7221
Gabriel Enström <gabriel.enstrom@gmail.com>
2020-12-14 16:55:16 +0100

    Populated treeview with data.

---
+++ Bokhandel/Models/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Bokhandel
+{
+    public class Order
+    {
+        public Order()
+        {
+            Orderdetaljers = new HashSet<Orderdetaljer>();
+        }
+
+        public int OrderId { get; set; }
+        public string KundId { get; set; }
+        public DateTime Orderdatum { get; set; }
+        public DateTime Leveransdatum { get; set; }
+
+        public virtual Kunder Kund { get; set; }
+        public virtual ICollection<Orderdetaljer> Orderdetaljers { get; set; }
+    }
+}
